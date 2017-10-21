eb43da91c618a4caf02967fed7a2d857d39ae3c1
Jonathan Wang <jonathzw@usc.edu>
2017-10-21 02:07:04 -0700

    Receiving texts added

---
+++ receive_sms.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=["GET", "POST"])
+def sms_reply():
+    resp = MessagingResponse()
+    resp.message("This is Uplift responding to you!")
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
