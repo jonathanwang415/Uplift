a3349ba64a0dd0335e3d375dd57432406316d277
Patrick Woo-Sam <pwoosam5@icloud.com>
2017-10-21 12:23:03 -0700

    styoe change for constants and function for sending twilio messages

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-account_sid = "AC4bb98a6a4f9b6dd25636a0397e0e14c0"
+ACCOUNT_SID = "AC4bb98a6a4f9b6dd25636a0397e0e14c0"
 # Your Auth Token from twilio.com/console
-auth_token = "dd12a9b3f8049168a9a68c8b190bd3ba"
+AUTH_TOKEN = "dd12a9b3f8049168a9a68c8b190bd3ba"
