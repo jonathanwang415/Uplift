c41fd63de228b2ef3ec50296cb2335e5d07fd4d0
Abi <abixcastro@gmail.com>
2017-10-22 03:21:57 -0700

    Fixed phenotype names

--- generate_database.py
+++ generate_database.py
@@ -49,7 +49,7 @@ with mdb.connect('localhost', 'root', 'toor', 'userdb') as cur:
     R_twoSug = 'Increase intensity and amount of physical activity on a daily basis'
     R_threeSug = 'Limit red and processed meats while making an attempt to eat more vegetables and fruits'
     R_fourSug = 'Avoid drinking too much acholoic beverages'
-    cur.execute(('INSERT INTO disease(phenotype_name, zero, one, two, three, four, s_one, s_two, s_three, s_four) VALUES(\'colorectal cancer\',' 
+    cur.execute(('INSERT INTO disease(phenotype_name, zero, one, two, three, four, s_one, s_two, s_three, s_four) VALUES(\'colorectal_cancer\',' 
                 '\'Not very prone to Colorectal Cancer\', \'Slightly prone to Colorectal cancer\', \'intermediatly prone to Colorectal Cancer\',' 
                 '\'Prone to colorectal cancer\', \'Highly prone to Colorectal cancer\',' 
                 '\'{}\', \'{}\', \'{}\', \'{}\')'.format(R_oneSug, R_twoSug, R_threeSug, R_fourSug)))
@@ -58,11 +58,20 @@ with mdb.connect('localhost', 'root', 'toor', 'userdb') as cur:
     P_twoSug = 'Eat more fish (evidence and studies suggest that fish can help protect against prostate cancer with good fats'
     P_threeSug = 'Incorporating more cooked tomatoes (cooked with olive) into the daily meals'
     P_fourSug = 'Seek medical treatment for stress, high blood pressure, diabetes, high cholesterol, and depression'
-    cur.execute(('INSERT INTO disease(phenotype_name, zero, one, two, three, four, s_one, s_two, s_three, s_four) VALUES(\'prostate cancer\',' 
+    cur.execute(('INSERT INTO disease(phenotype_name, zero, one, two, three, four, s_one, s_two, s_three, s_four) VALUES(\'prostate_cancer\',' 
                 '\'Not very prone to Prostate Cancer\', \'Slightly prone to Prostate Cancer\', \'intermediatly prone to Prostate Cancer\',' 
                 '\'Prone to Prostate Cancer\', \'Highly prone to Prostate Cancer\',' 
                 '\'{}\', \'{}\', \'{}\', \'{}\')'.format(P_oneSug, P_twoSug, P_threeSug, P_fourSug)))
 
+    P_oneSug = 'Watch your calcium intake and do not take supplemental doses far about the recommended daily allowance'
+    P_twoSug = 'Eat more fish (evidence and studies suggest that fish can help protect against prostate cancer with good fats'
+    P_threeSug = 'Incorporating more cooked tomatoes (cooked with olive) into the daily meals'
+    P_fourSug = 'Seek medical treatment for stress, high blood pressure, diabetes, high cholesterol, and depression'
+    cur.execute(('INSERT INTO disease(phenotype_name, zero, one, two, three, four, s_one, s_two, s_three, s_four) VALUES(\'type_2_diabetes\',' 
+                '\'Not very prone to Prostate Cancer\', \'Slightly prone to Prostate Cancer\', \'intermediatly prone to Prostate Cancer\',' 
+                '\'Prone to Prostate Cancer\', \'Highly prone to Prostate Cancer\',' 
+                '\'{}\', \'{}\', \'{}\', \'{}\')'.format(P_oneSug, P_twoSug, P_threeSug, P_fourSug)))
+    
     cur.execute(('CREATE TABLE personality(id INT PRIMARY KEY AUTO_INCREMENT, phenotype_name VARCHAR(150), zero VARCHAR(100),'
                 'one VARCHAR(100), two VARCHAR(100), three VARCHAR(100), four VARCHAR(100), s_one VARCHAR(1000), s_two VARCHAR(1000),' 
                 's_three VARCHAR(1000), s_four VARCHAR(1000) )'))
