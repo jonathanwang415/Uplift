c6ec53db59ba3a60b125130c2cf1e4950b68f7e9
Jonathan Wang <jonathzw@usc.edu>
2017-10-21 20:35:10 -0700

    Added category parsing for incoming text messages

--- process_response.py
+++ process_response.py
@@ -1,14 +1,42 @@
-categories = [1, 2, 3, 4, 5]
+categoriesCommands = [1, 2, 3, 4, 5]
+categories = ["traits", "personality", "food_and_nutrition", "allergies", "diseases"]
 
 def getReply(body):
-    if body in categories:
-        return "You choose category: " + body
-    else:
-        return body + "Not a valid category. Please enter a category from 1 - 6."
-        + "\nHere are the following categories: "
-        + "\n1 - Physical Traits: "
-        + "\n2 - Personality: "
-        + "\n3 - Food and Nutrition: "
-        + "\n4 - Allergies: "
-        + "\n5 - Disease: "
-
+    try: 
+        cmd = int(body)
+        if cmd in categoriesCommands:
+            # TODO: Add more responses here.
+            if cmd is 1:
+                return "You choose category: " + categories[cmd-1]
+            elif cmd is 2:
+                return "You choose category: " + categories[cmd-1]
+            elif cmd is 3:
+                return "You choose category: " + categories[cmd-1]
+            elif cmd is 4:
+                return "You choose category: " + categories[cmd-1]
+            else:
+                return "You choose category: " + categories[cmd-1]
+        else:
+            return body + " is not a valid category."\
+            "\nPlease enter a category from 1 - 6." \
+            "\nHere are the following categories: " \
+            "\n1 - Physical Traits: " \
+            "\n2 - Personality: " \
+            "\n3 - Food and Nutrition: " \
+            "\n4 - Allergies: " \
+            "\n5 - Disease: "
+   
+    except ValueError:
+        # Set up code here
+        if body is "start":
+            return "Hi! Welcome to Uplift."\
+            "\nPlease enter a category from 1 - 6." \
+            "\nHere are the following categories: " \
+            "\n1 - Physical Traits: " \
+            "\n2 - Personality: " \
+            "\n3 - Food and Nutrition: " \
+            "\n4 - Allergies: " \
+            "\n5 - Disease: "
+        # More elif here
+        else:    
+            return "New command I haven't added yet: " + body
